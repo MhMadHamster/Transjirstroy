77a34b6d26f4de73ceb2b230ddf46e960f1fbd83
Max <mhmad.hamster@gmail.com>
2015-04-17 16:19:09 +0300

    index.htmk + styles

--- app/js/main.js
+++ app/js/main.js
@@ -16,5 +16,30 @@ $(document).ready(function() {
         slider.goToNextSlide();
     })
 
+    $('.thumb-list__item').on('click', function(e) {
+
+        e.preventDefault();
+
+        if(!$(this).hasClass('active')) {
+
+            var currentThumb = $(this);
+            var currentPersona = $(this).attr('href');
+
+            $(this).closest('.thumb-list')
+                   .find('.thumb-list__item.active')
+                   .removeClass('active')
+                   .closest('.personal-list')
+                   .find('.personal-list__item.active')
+                   .removeClass('active')
+                   .fadeOut(300, function() {
+                        currentThumb.addClass('active')
+                                    .closest('.personal-list')
+                                    .find('.personal-list__item' + currentPersona)
+                                    .fadeIn(300)
+                                    .addClass('active');
+                   });
+        }
+
+    });
 
 })
